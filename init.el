bffa1f9330e916f527716cf44ba00a5b8a18fa35
Timofey Alekseev <atimofey@mail.ru>
2024-05-04 17:16:16 +0300

    fix package init

--- init.el
+++ init.el
@@ -1,10 +1,2 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
-
 ;; FIXME Нужен более адекватный спсоб определить путь до init.el.org
 (org-babel-load-file (expand-file-name "~/.emacs.d/init.el.org"))
